93c14b96a6e433bcbe3622f31edde58aef179b34
Tenkaklet <tenkaklet@gmail.com>
2015-10-17 13:36:47 +0200

    fixed mobile version

--- js/master.js
+++ js/master.js
@@ -3,13 +3,41 @@
 var $header = $('header');
 var $weather = $('#weather');
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 $(function() {
 
 	//display none on weather
 
 	$weather.hide();
-	// animation of the Top Form	
-	$('form').one('submit', function() {
+	// animation of the Top Form
+
+	if(isMobile.any() ) mobile();
+	else {
+		desktop();
+	}
+
+	function desktop () {
+		$('form').one('submit', function() {
 		$header.animate({				
 			'marginTop': "-=220px"},
 			1000, function() {
@@ -18,7 +46,10 @@ $(function() {
 		}); // end animate
 
 	}); // clicker - one
-	$('.btn-phone').click(function () {
+	}
+	function mobile () {
+		$('form').one('submit', function () {
 		$weather.show(500);
 	});
+	}
 });
